8bcb425a9d86d8bda1aef3e4959ad0266b86cfa6
punit369 <punit.dd@somaiya.edu>
2022-08-15 11:46:32 +0530

    first commit

---
+++ src/training_placement_mgmt/JOptionPane.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package training_placement_mgmt;
+
+/**
+ *
+ * @author Ruchi Manish Davda
+ */
+class JOptionPane {
+
+    static void showMessageDialog(Object object, Exception e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void showMessageDialog(Object object, String record_Not_found) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
